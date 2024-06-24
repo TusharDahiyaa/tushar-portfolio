3b126a1b1eb46004d401e9e8759f86c5801f6677
Tushar Dahiya <tushardahiya202@gmail.comclear>
2024-06-24 14:01:02 +0530

    Removed few animations and made site smoother

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { WavyBackground } from "./ui/wavy-background";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
 import projectsData from "./ProjectsData";
 import { BackgroundGradient } from "./ui/background-gradient";
