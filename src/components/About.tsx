8d0884c0699c44c8bd78ff08c71c3010aba10e48
Tushar Dahiya <tushardahiya202@gmail.comclear>
2024-06-28 17:10:03 +0530

    Better UI

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,8 @@ const About: React.FC = () => {
     <div className="text-center w-[80%] md:w-[60%] mx-auto">
       <h1 className=" text-3xl md:text-5xl my-10 font-bold">About Me 🧘‍♂️</h1>
       <p className="md:text-lg text-pretty mb-10">
-        Hello there! 👋 I'm Tushar Dahiya, your friendly neighborhood full-stack
-        web developer hailing from India. By day, I'm a freelancer armed with
-        skills honed in the illustrious{" "}
+        Hey there! 👋 I'm Tushar Dahiya, a full-stack web developer from India.
+        By day, I'm a freelancer, and I've sharpened my skills at the{" "}
         <a
           href="https://www.credential.net/79667545-557f-46c8-91d0-d6e58e0edf0f"
           target="_blank"
@@ -15,24 +14,24 @@ const About: React.FC = () => {
         >
           MERN stack bootcamp from Upgrad.
         </a>{" "}
-        Currently, I'm navigating the coding cosmos as part of the 100xdevs
-        Cohort, soaking up knowledge like a sponge in a data ocean. Solving
-        problems is my jam; there's nothing quite like that eureka moment when
-        everything falls into place.
+        Right now, I'm diving deep into the coding world as part of the 100xdevs
+        Cohort, soaking up knowledge like a sponge. I love that eureka moment
+        when everything clicks into place while solving problems.
       </p>
       <p className="md:text-lg text-pretty mb-10">
-        My tech arsenal includes{" "}
+        My tech toolkit includes{" "}
         <span className="font-mono bg-zinc-500 bg-opacity-40 px-1 rounded-lg">
           React, Next.js, Node.js and MongoDB/PostgreSQL, with TypeScript and
           Prisma
         </span>{" "}
-        adding that extra sprinkle of magic. I'm always on the prowl for new
-        tech to sink my teeth into. I'm also on the lookout for a full-time gig
-        as a full-stack developer, I'm ready to bring my A-game to any team.
+        adding that extra touch of magic. I'm always on the hunt for new tech to
+        explore. Currently, I'm on the lookout for a full-time gig as a
+        full-stack developer and ready to bring my A-game to any team.
       </p>
       <p className="md:text-lg text-pretty mb-10">
-        When I'm not coding, I'm gaming, movie-binging, or hanging out. Just
-        like curious George, I'm always eager for the next tech adventure. 🐒✨
+        When I'm not coding, you'll find me gaming, binging on movies, or just
+        hanging out. Like Curious George, I'm always excited for the next tech
+        adventure. 🐒✨
       </p>
     </div>
   );
